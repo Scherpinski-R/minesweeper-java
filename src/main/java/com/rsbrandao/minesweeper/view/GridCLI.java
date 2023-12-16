b86fdff4c2833dae018487df624d6cb44403b94d
Raphael Scherpinski <rsbrandao@inf.ufrgs.br>
2023-12-16 14:25:02 -0300

    VIEW cli with GridCLI class.

---
+++ src/main/java/com/rsbrandao/minesweeper/view/GridCLI.java
@@ -0,0 +1,72 @@
+package com.rsbrandao.minesweeper.view;
+
+import com.rsbrandao.minesweeper.exception.ExplosionException;
+import com.rsbrandao.minesweeper.exception.QuitException;
+import com.rsbrandao.minesweeper.model.Grid;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class GridCLI {
+    public GridCLI(Grid grid) {
+        this.grid = grid;
+        playGame();
+    }
+
+    private void playGame() {
+        try {
+            boolean leave = false;
+            while (!leave) {
+                loop();
+                String answer = prompt("Keep playing? (Y/n): ");
+                if ("n".equalsIgnoreCase(answer))
+                    leave = true;
+                else grid.restart();
+            }
+            System.out.println("Bye, Bye!");
+
+        } catch (QuitException e) {
+            System.out.println("Bye, Bye!");
+        } finally {
+            in.close();
+        }
+    }
+
+    private void loop() {
+        try {
+            while (!grid.goalCompleted()) {
+                System.out.println(grid);
+                String answer = prompt("Type the coordinates (x,y): ");
+                Iterator<Integer> xy = Arrays.stream(answer.trim().split(",")).map(Integer::parseInt).iterator();
+
+                answer = prompt("1 - Open | 2 - (Un)Flag: ");
+
+                if ("1".equals(answer)) {
+                    grid.open(xy.next(), xy.next());
+                } else if ("2".equals(answer)) {
+                    grid.changeFlag(xy.next(), xy.next());
+                }
+            }
+
+            System.out.println(grid);
+            System.out.println("You won!");
+        } catch (ExplosionException e) {
+            System.out.println(grid);
+            System.out.println("You Lost!");
+        }
+    }
+    private String prompt(String text) {
+        System.out.print(text);
+        String answer = in.nextLine();
+
+        if ("quit".equalsIgnoreCase(answer)) {
+            throw new QuitException();
+        }
+
+        return answer;
+    }
+
+    private static Grid grid;
+    private Scanner in = new Scanner(System.in);
+}
